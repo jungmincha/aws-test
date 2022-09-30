0191f92fee90e3df520000e4485a44fe5947f281
jmcha <jmcha@cyglobal.net>
2022-09-30 10:34:16 +0900

    build 파일도 git에 올리기

--- vue/vue-front/config/index.js
+++ vue/vue-front/config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/api': {
+          // 프록시 요청을 보낼 서버의 주소
+          target: 'http://localhost:8585',
+          changeOrigin: true
+        }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
