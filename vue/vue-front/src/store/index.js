bdea38a5b20da51386a96919b98df4f566353c6a
jungmincha <prttyjimmy@naver.com>
2022-10-03 01:05:46 +0900

    토큰 값 넘겨받기

--- vue/vue-front/src/store/index.js
+++ vue/vue-front/src/store/index.js
@@ -21,13 +21,17 @@ const store = new Vuex.Store({
   },
   mutations: { //state 값을 변화시켜 준다.
     //로그인이 성공 했을 때
-    loginSuccess(state, payload, token) {
+    loginSuccess(state, payload) {
+      let accessToken = payload.accessToken
+      let userInfo = {
+        username: payload.username,
+        nickname: payload.nickname,
+        authorityName: payload.authorityName
+      }
       state.isLogin = true
       state.isLoginError = false
-      state.accessToken = token
-      state.userInfo = payload
-      console.log('loginSuccess mutation')
-      console.log(state.isLogin)
+      state.accessToken = accessToken
+      state.userInfo = userInfo
     },
     loginError(state) {
       state.isLogin = false
@@ -54,12 +58,11 @@ const store = new Vuex.Store({
           let userInfo = {
             username: response.data.username,
             nickname: response.data.nickname,
-            authorityName: response.data.authorityName
+            authorityName: response.data.authorityName,
+            accessToken: token
           }
-          console.log('login 성공했을 때...')
-          console.log(userInfo)
           //토큰 정보가 유효하면 state값을 변화시켜주는 mutations의 loginSuccess에 값을 넣어준다.
-          commit('loginSuccess', userInfo, token)
+          commit('loginSuccess', userInfo)
         })
         .catch(error => {
           console.log(error)
@@ -73,7 +76,6 @@ const store = new Vuex.Store({
       return state.accessToken
     },
     isLogin: state => {
-      console.log('getters state', state.isLogin)
       return state.isLogin
     },
     getUserInfo: state => {
